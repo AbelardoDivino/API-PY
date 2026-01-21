f66ef959e8bfe0c01f191d8e7ef5bad0a0e023ec
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-01-21 18:48:32 -0300

    .

--- server/server.py
+++ server/server.py
@@ -1,37 +1,39 @@
-from   http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-import hashlib #gerar o tokem
+import hashlib
 
-host  = "localhost"
-
+host = "localhost"
 PORT = 8000
+TOKENS = {}
 
-TOKENS = {} #gerar os tokens para armazenar na autenticaçao
-
-def gerar_tokens(usuario):
+def gerar_token(usuario):
     return hashlib.sha256(usuario.encode()).hexdigest()
 
 class API(BaseHTTPRequestHandler):
     def do_POST(self):
-        #vereficar se a requsiçao e para o endipoint / login
         if self.path == "/login":
-            tamanho = int(self.headers["content-length"]) # ler o tamanho do corpo da rquisiçao 
+            tamanho = int(self.headers["Content-Length"])
             corpo = self.rfile.read(tamanho)
-            dados = self.rfile.loads(corpo)
+            dados = json.loads(corpo.decode())
 
             usuario = dados.get("user")
-            senha = dados.get("passoword")
+            senha = dados.get("password")
 
             with open("users.json") as f:
                 users = json.load(f)
-            
+
             if usuario in users and users[usuario]["password"] == senha:
-                token = gerar_tokens(usuario)
+                token = gerar_token(usuario)
                 TOKENS[token] = users[usuario]["role"]
 
-            self.send_response(200)
-            self.send_header("content-Type", "application/json")
-            self.end_headers()
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
+                self.end_headers()
+                self.wfile.write(json.dumps({"token": token}).encode())
+            else:
+                self.send_response(401)
+                self.end_headers()
+
 
             self.wfile.write(json.dumps({
                 "token": token
